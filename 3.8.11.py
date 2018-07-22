5a5e2da56723fa4237a13f2621000de2068597fd
Scott Nonweiler <nonweisb@whitman.edu>
2018-07-21 19:05:55 -0700

    First commit

---
+++ 3.8.11.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 13:32:58 2018
+
+@author: XPS 15
+"""
+
+import turtle
+wn=turtle.Screen()
+tess=turtle.Turtle()
+
+for i in range(5):
+    tess.forward(100)
+    tess.right(144)
+    
+tess.hideturtle()
+    
+wn.mainloop()
